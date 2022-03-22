40a43eed48ebaa6eb6727941d8dd4afca0d1a4d8
JBEDU09 <JBEDU09@DESKTOP-69PS1OA>
2022-03-22 10:24:44 +0900

    555

---
+++ src/hi.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scnanner scnanner = new Scanner(System.in);
+
+System.out.print("두자리의 정수를 입력하시요:");
+int  score = scanner.nextint();
+
+if (score/10=a) {System.out.printin("a")
+int a=score/10;	
+if (score%10=b){System.out.print("b")
+	System.out.printin("b")
+if(a=b)	
+{System.out.printin("십의 자리와 일의 자리의 수가 같습니다")	};
+
+scanner.close();
+}
+
+
+
+
